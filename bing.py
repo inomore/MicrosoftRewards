b4439c9a22d0d4ac900c4e7a75ce134c0adbf74d
zengfu94 <zengfu94@gmail.com>
2017-09-13 18:48:07 -0500

    fix UAs, fully working report

--- bing.py
+++ bing.py
@@ -32,11 +32,11 @@ import urllib
 import time
 import urllib3
 import re
+import os
 from multiprocessing import Pool
 from random import randint
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-report = {"redeem_ready" : [], "not_ready" : []}
 
 def safe_print(content):
     print "{0}\n".format(content),
@@ -52,8 +52,10 @@ def search_account(account):
 		proxy = account.split(">")[4]
 	except IndexError:
 		proxy = "127.0.0.1:8080"
-	headers = c.headers
-	headers["User-Agent"] = desktop_ua
+	mobile_headers = c.headers
+	mobile_headers["User-Agent"] = mobile_ua
+	desktop_headers = c.headers
+	desktop_headers["User-Agent"] = desktop_ua
 	data = {"i13":"0", "type":"11", "LoginOptions":"3", "lrt":"", "ps":"2", "psRNGCDefaultType":"", "psRNGCEntropy":"", "psRNGCSLK":"", "canary":"", "ctx":"", "NewUser":"1", "FoundMSAs":"", "fspost":"0", "i21":"0", "i2":"1", "i17":"0", "i18":"__ConvergedLoginPaginatedStrings%7C1%2C__ConvergedLogin_PCore%7C1%2C", "i19":"2" + str(randint(0, 5000))}
 	proxies = {"http":"127.0.0.1:8080", "https":"127.0.0.1:8080"}
 	data["login"] = email
@@ -66,15 +68,15 @@ def search_account(account):
 	time.sleep(wait_secs)
 
 	#login mobile and desktop
-	res = requests.get(c.hostURL, headers=headers)
-	headers["Referer"] = res.url
+	res = requests.get(c.hostURL, headers=desktop_headers)
+	desktop_headers["Referer"] = res.url
 	index = res.text.index("WindowsLiveId")
 	cutText = res.text[index + 16:]
 	loginURL = cutText[:cutText.index("\"")]
 	loginURL = bytes(loginURL).encode("utf-8").decode("unicode_escape")
-	headers["Host"] = c.loginHost
-	res = requests.get(loginURL, headers=headers)
-	headers["Referer"] = res.url
+	desktop_headers["Host"] = c.loginHost
+	res = requests.get(loginURL, headers=desktop_headers)
+	desktop_headers["Referer"] = res.url
 	cookies = res.cookies
 	cookies["CkTst"] = "G" + str(int(time.time() * 1000))
 	index = res.text.index(c.loginPostURL)
@@ -87,26 +89,25 @@ def search_account(account):
 	PPSX = 'PassportRN'[:-random.randint(0,9)]
 	data["PPSX"] = PPSX
 	cookies["wlidperf"] = "FR=L&ST=" + str(int(time.time() * 1000))
-	res = requests.post(postURL, cookies=cookies, data=data, headers=headers)
-	headers["Referer"] = res.url
+	res = requests.post(postURL, cookies=cookies, data=data, headers=desktop_headers)
+	desktop_headers["Referer"] = res.url
 	form = BS(res.content, "html.parser").findAll("form")[0]
 	params = dict()
 	for field in form:
 		params[field["name"]] = field["value"]
-	headers["Host"] = c.host
-	res = requests.post(form.get("action"), cookies=cookies, data=params, headers=headers)
-	headers["Referer"] = res.url
+	desktop_headers["Host"] = c.host
+	res = requests.post(form.get("action"), cookies=cookies, data=params, headers=desktop_headers)
+	desktop_headers["Referer"] = res.url
 	cookies = res.cookies
 	safe_print(email + ": logged in")
 	finder = re.compile("'(\d+)'")
-	page = requests.get("https://www.bing.com/rewardsapp/reportActivity", cookies=cookies, headers=headers)
+	page = requests.get("https://www.bing.com/rewardsapp/reportActivity", cookies=cookies, headers=desktop_headers)
 	oldPoints = int(finder.search(page.content).group(1))
 	safe_print(email + ": current points: " + str(oldPoints))
 
 	#parse rewards
-	flyout = headers
-	flyout["User-Agent"] = mobile_ua
-	page = requests.get("http://www.bing.com/rewardsapp/flyoutpage/?style=v2", cookies=cookies, headers=flyout, verify=False)
+	mobile_headers["User-Agent"] = mobile_ua
+	page = requests.get("http://www.bing.com/rewardsapp/flyoutpage/?style=v2", cookies=cookies, headers=mobile_headers, verify=False)
 	soup = BS(page.content,"html.parser")
 	rewards = soup.findAll("ul",{"class" : "item"})
 	extra_offers = []
@@ -135,10 +136,6 @@ def search_account(account):
 	except UnboundLocalError:
 		safe_print(email + ": failed to login")
 		return
-	headers = {"desktop" : headers, "mobile" : headers}
-	headers["mobile"]["User-Agent"] = mobile_ua
-	print mobile_ua
-	headers["desktop"]["User-Agent"] = desktop_ua
 
 	#searches throughout the period of time 5.5-8.3 hours default
 	querytime = random.randint(c.querytime_low,c.querytime_high)
@@ -154,20 +151,25 @@ def search_account(account):
 				printed = True
 		except ValueError:
 			safe_print(email + ": searches done")
-			page = requests.get("https://www.bing.com/rewardsapp/reportActivity", cookies=cookies, headers=headers["desktop"])
+			desktop_headers["User-Agent"] = desktop_ua
+			page = requests.get("https://www.bing.com/rewardsapp/reportActivity", cookies=cookies, headers=desktop_headers)
 			newPoints = int(finder.search(page.content).group(1))
 			safe_print(email + ": points earned: " + str(newPoints - oldPoints))
 			safe_print(email + ": total points: " + str(newPoints))
 			if newPoints >= c.redeem_ready:
-				report["redeem_ready"].append({email : {"earned" : newPoints - oldPoints, "total" : newPoints}})
+				redeem_ready = open("redeem_ready.txt","a+")
+				redeem_ready.write(str({email : {"earned" : newPoints - oldPoints, "total" : newPoints}}))
+				redeem_ready.close()
 			else:
-				report["not_ready"].append({email : {"earned" : newPoints - oldPoints, "total" : newPoints}})
+				not_ready = open("not_ready.txt","a+")
+				not_ready.write(str({email : {"earned" : newPoints - oldPoints, "total" : newPoints}}))
+				not_ready.close()
 			return
 		if i in querytimes:
 			if mobile_searches > mobile_left and desktop_searches > desktop_left and len(extra_offers) > 0:
 				offer = random.choice(extra_offers)
 				extra_offers.remove(offer)
-				requests.get("https://bing.com" + offer, cookies=cookies, headers=headers["desktop"])
+				requests.get("https://bing.com" + offer, cookies=cookies, headers=desktop_headers)
 			elif desktop_searches > desktop_left and mobile_searches < mobile_left:
 				lasttype = "mobile"
 			elif desktop_searches < desktop_left and mobile_searches > mobile_left:
@@ -195,11 +197,12 @@ def search_account(account):
 			query = str(gen.generateQueries(1,set()).pop())
 			if "desktop" in lasttype:
 				desktop_searches += 1
-				requests.get(c.searchURL + query, cookies=cookies, headers=headers["desktop"])
+				desktop_headers["User-Agent"] = desktop_ua
+				requests.get(c.searchURL + query, cookies=cookies, headers=desktop_headers)
 			if "mobile" in lasttype:
 				mobile_searches += 1
-				requests.get(c.searchURL + query, cookies=cookies, headers=headers["mobile"])
-				print headers["mobile"]["User-Agent"]
+				mobile_headers["User-Agent"] = mobile_ua
+				requests.get(c.searchURL + query, cookies=cookies, headers=mobile_headers)
 			safe_print(email + ": " + lasttype + " search: " + query)
 			printed = False
 if __name__ == "__main__":
@@ -216,8 +219,15 @@ if __name__ == "__main__":
 	pool.map(search_account, accounts)
 	output_file = open("report.txt","w+")
 	output_file.write("---REDEEM READY---\n")
-	for account in report["redeem_ready"]:
-		output_file.write(account + "\n")
+	redeem_ready = open("redeem_ready.txt","r")
+	for line in redeem_ready:
+		output_file.write(line + "\n")
 	output_file.write("---NOT READY---\n")
-	for account in report["not_ready"]:
-		output_file.write(account + "\n")
+	not_ready = open("not_ready.txt","r")
+	for line in not_ready:
+		output_file.write(line + "\n")
+	output_file.close()
+	redeem_ready.close()
+	not_ready.close()
+	os.remove("not_ready.txt")
+	os.remove("redeem_ready.txt")
